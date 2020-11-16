ba886076b79951e36a50f32ef0f1d84d2af31c63
armin nouri <arminoori52@yahoo.com>
2020-11-16 14:26:38 +0330

    add file

---
+++ test.bat
@@ -0,0 +1,10 @@
+	 	 	 	c	c	 	 	R	c	s
+	R	c	 	 	s	 	 	R	R	 
+	 	s	 	 	 	 	R	s	 	 
+	 	R	 	 	 	 	c	 	 	 
+	 	 	s	s	 	 	R	R	R	 
+	 	 	 	c	 	 	s	 	 	 
+	c	 	 	 	 	 	c	 	 	 
+	 	s	R	 	 	 	 	 	 	 
+	s	 	c	 	 	 	 	c	s	s
+	 	 	R	 	 	 	 	 	c	 
